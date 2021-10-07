1ad2595ede09de057832b77391b931f56c33a928
Alequita <71567007+Alequita@users.noreply.github.com>
2021-10-07 09:06:42 -0300

    More stuff thats not going to be kept

--- cloudagro/cloudagro/settings.py
+++ cloudagro/cloudagro/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
-    'purchases.apps.PurchasesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
