051bbafe3e2efb2afbe4f7ef628514606b958d12
Alequita <71567007+Alequita@users.noreply.github.com>
2021-10-07 09:13:21 -0300

    bad stuff

--- cloudagro/purchases/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Animals, Purchases
-
-admin.site.register(Animals)
-admin.site.register(Purchases)
--- cloudagro/purchases/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import models
-from django.forms import ModelForm, fields
-from .models import Purchases
-
-class PurchaseForm(ModelForm):
-    class Meta: 
-        model = Purchases
-        fields = '__all__'
