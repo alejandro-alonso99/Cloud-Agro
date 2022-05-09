418a86ed1aab16669286f411dcf24cffb5a6c714
Alequita <alejandroalonso0306@gmail.com>
2022-05-09 16:25:49 -0300

    staticfiles commit

--- cloudagro/settings.py
+++ cloudagro/settings.py
@@ -145,10 +145,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'account/static')
 ]
 
 LOGIN_REDIRECT_URL = 'account:dashboard'
